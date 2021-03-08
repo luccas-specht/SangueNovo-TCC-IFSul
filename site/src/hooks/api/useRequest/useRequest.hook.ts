28e361f2ba068fb38b8751ca7de954f4c9e9edb8
luccas-specht <luccasspecht70@gmail.com>
2021-03-07 23:38:15 -0300

    add

--- site/src/hooks/api/useRequest/useRequest.hook.ts
+++ site/src/hooks/api/useRequest/useRequest.hook.ts
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+
 import { AxiosRequestConfig } from "axios";
 
 import { methodsAvaibles } from "../../../constants";
@@ -17,25 +19,33 @@ export const useRequest = (path: string): Response => {
   const { request } = useAxiosApiSangueNovo();
   const { user } = useAuthenticated();
 
-  const buildUrl = (url?: string) => (url ? `${path}/${url}` : path);
+  const buildUrl = useCallback(
+    (url?: string) => (url ? `${path}/${url}` : path),
+    [path]
+  );
 
-  const buildHeaders = () => ({
-    Authorization: `Bearer ${user?.token}`,
-  });
+  const buildHeaders = useCallback(() => {
+    return {
+      Authorization: `Bearer ${user?.token}`,
+    };
+  }, [user?.token]);
 
-  const callApi = async (method: string, url?: string, data?: any) => {
-    const requestConfig = {
-      data: data,
-      method: method,
-      url: buildUrl(url),
-      headers: buildHeaders(),
-    } as AxiosRequestConfig;
-    try {
-      return await request(requestConfig);
-    } catch (err) {
-      return err.response;
-    }
-  };
+  const callApi = useCallback(
+    async (method: string, url?: string, data?: any) => {
+      const requestConfig = {
+        data: data,
+        method: method,
+        url: buildUrl(url),
+        headers: buildHeaders(),
+      } as AxiosRequestConfig;
+      try {
+        return await request(requestConfig);
+      } catch (err) {
+        return err.response;
+      }
+    },
+    [buildUrl, buildHeaders, request]
+  );
 
   return {
     get: async (url?: string, data?: any): Promise<any> =>
