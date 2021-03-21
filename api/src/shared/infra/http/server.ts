e1020af971703b0fade5bd58bf0670f758728f6c
luccas-specht <luccasspecht70@gmail.com>
2021-03-21 15:02:43 -0300

    add

--- api/src/shared/infra/http/server.ts
+++ api/src/shared/infra/http/server.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import { routes } from './routes';
-import { uploadConfig } from '@config/upload'
+import { uploadConfig } from '@config/upload';
 import { AppError } from '@shared/errors/appError';
 
 import '@shared/infra/typeorm';
@@ -15,26 +15,28 @@ import '@shared/container';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-app.use((error: Error, resquest: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message
-    })
-  }
-  console.error(error);
+app.use(
+  (error: Error, resquest: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.error(error);
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  })
-});
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log('Server Started in port: 3333');
