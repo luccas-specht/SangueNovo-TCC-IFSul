3510b174195e10c023dd81016fb80dc0de8adb61
Luccas Specht <luccasspecht70@gmail.com>
2021-06-01 00:02:22 -0300

    mapper donations

--- api/src/modules/user/donator/services/ListDonationsByDonatorIdService.ts
+++ api/src/modules/user/donator/services/ListDonationsByDonatorIdService.ts
@@ -7,6 +7,7 @@ import { IDonatorRepository } from '../IRepository/IDonatorRepository';
 import { AppDonation } from '@modules/donation/infra/typeorm/entities/AppDonation';
 import { IDonationRepository } from '@modules/donation/IRepository/IDonatitonRepository';
 import { DonationStatus } from '@modules/donation/infra/typeorm/entities/EnumDonationStatus';
+import { IInstitutionRepository } from '@modules/user/institution/IRepository/IInstitutionRepository';
 
 interface Request {
   id: string;
@@ -19,7 +20,10 @@ export class ListDonationsByDonatorId {
     private donatorRepository: IDonatorRepository,
 
     @inject('DonationRepository')
-    private donationRepository: IDonationRepository
+    private donationRepository: IDonationRepository,
+
+    @inject('InstitutionRepository')
+    private institutionRepository: IInstitutionRepository
   ) {}
 
   public async execute({ id }: Request): Promise<AppDonation[]> {
@@ -31,7 +35,31 @@ export class ListDonationsByDonatorId {
         donator.id,
         DonationStatus.FINISHED
       );
+    return donations.length > 0 ? this.mapperDonations(donations) : [];
+  }
 
-    return donations;
+  private async mapperDonations(donations: AppDonation[]): Promise<any[]> {
+    const institutions = await this.institutionRepository.findAll();
+
+    return donations.map((donation) => ({
+      status: donation.donationStatus,
+      donator: {
+        name: donation.donator.name,
+        date_last_donation: donation.donator.date_last_donation,
+      },
+      campaign: {
+        avatar: donation.campaign.avatar,
+        title: donation.campaign.title,
+        description: donation.campaign.description,
+        bloodType: donation.campaign.typeBlood,
+        institution: {
+          razaoSocial: institutions.find((institution) =>
+            institution.campaigns.map(
+              (campaign) => campaign.id === donation.campaign.id
+            )
+          )?.razao_social,
+        },
+      },
+    }));
   }
 }
